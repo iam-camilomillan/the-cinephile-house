e4e1d49ef6adbdb69ba0991369ecf5d09c35187e
iam-camilomillan <iam.camilomillan@gmail.com>
2023-09-26 17:18:38 -0500

    Firebase config file updated.

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,10 +1,12 @@
+/* Firebase imports */
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-/* Env variables import */
+/* Env variables imports */
 import { env } from "~/env.mjs";
 
+/* Firebase project config */
 const firebaseConfig = {
   apiKey: env.NEXT_PUBLIC_FIREBASE_API,
   authDomain: "the-cinephile-house-cm.firebaseapp.com",
@@ -14,8 +16,10 @@ const firebaseConfig = {
   appId: "1:1088974824285:web:c9876225b07785887bbeb4",
 };
 
+/* Firebase app initialization */
 const firebaseApp = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(firebaseApp);
 const firebaseFirestore = getFirestore(firebaseApp);
 
+/* Firebase exports */
 export { firebaseAuth, firebaseFirestore };
