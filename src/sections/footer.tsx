2f192e743f279a4fcee9db8d5cce4d5f1310e5ee
iam-camilomillan <iam.camilomillan@gmail.com>
2023-10-07 14:05:06 -0500

    Footer updated.

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -1,23 +1,20 @@
 const Footer = () => {
   return (
-    <footer className="">
-      <p
-        className="mx-auto max-w-5xl border-t
-      px-8 py-6 text-center"
-      >
-        <span className="opacity-80">Built by </span>
+    <footer>
+      <p className="mx-auto max-w-5xl border-t px-8 py-6 text-center">
+        <span className="opacity-90">Built by </span>
         <a
           href="https://iamcamilomillan.vercel.app/"
           target="_blank"
-          className="custom-underline font-medium opacity-80 duration-200 ease-in-out hover:opacity-100"
+          className="custom-underline font-medium opacity-90 duration-200 ease-in-out after:bg-clr-one hover:opacity-100"
         >
           Camilo Millan
         </a>{" "}
-        <span className="opacity-80">&#8226;</span>{" "}
+        <span className="opacity-90">&#8226;</span>{" "}
         <a
           href="https://github.com/iam-camilomillan/the-cinephile-house"
           target="_blank"
-          className="custom-underline font-medium opacity-80 duration-200 ease-in-out hover:opacity-100"
+          className="custom-underline font-medium opacity-90 duration-200 ease-in-out after:bg-clr-one hover:opacity-100"
         >
           Project repository
         </a>
