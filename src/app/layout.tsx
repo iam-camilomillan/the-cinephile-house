afcc2c55bb99bee73267368b40cc34426215f014
iam-camilomillan <iam.camilomillan@gmail.com>
2023-10-26 09:56:28 -0500

    Layout change.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`light ${inter.className} dark`}>
+    <html lang="en" className={`dark ${inter.className}`}>
       <body>
         <Providers>
           {/* Navbar */}
